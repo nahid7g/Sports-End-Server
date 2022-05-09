3029aff77352f6c645739d5551866705c608d4b3
Nahid Hasan <nahidhasankiran2@gmail.com>
2022-05-09 19:52:44 +0600

    Product loaded from mongodb

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const express = require("express");
 const cors = require("cors");
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
 // MiddleWare
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // Use Express
-app.get("/",(req,res) => {
-	res.send("Running perfectly!")
-})
-app.listen(port,() => {
-	console.log("Running from port:",port)
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.epqze.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri);
+async function run()  {
+	try {
+		await client.connect();
+		const productCollection =client.db("SportsEnd").collection("product");
+		app.get("/products",async(req,res) => {
+			const query = {};
+			const cursor = productCollection.find(query)
+			const products = await cursor.toArray();
+			res.send(products)
+		})
+	}
+	finally{}
+}
+run().catch(console.dir)
+
+app.get("/", (req, res) => {
+	res.send("Running perfectly!");
+});
+app.listen(port, () => {
+	console.log("Running from port:", port);
+});
+
